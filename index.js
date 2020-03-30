0d6ba40fb21cf177f7639ad06aea0f13e76d0dbb
patient-computer-3602 <thelionking898@gmail.com>
2020-03-30 14:39:07 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function add(a,b) {
+  return a+b;
+}
+
+function subtract(a,b) {
+  return b-a;
+}
+
+function multiply(a,b) {
+  return a*b;
+}
+
+function devide(a,b) {
+  return a/b;
+}
+
+function incerement(n) {
+  return n + 1;
+}
+
